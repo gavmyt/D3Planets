2c41d94ec93c9eb52e0f6c6daa4817f70ce03c17
Christian Landaverde <christian.landaverde@centre.edu>
2019-10-08 15:47:40 -0400

    work

--- planets.js
+++ planets.js
@@ -1,3 +1,35 @@
+var names = ["a", "b", "c"]
+    d3.select("#A1")
+        .append("p")
+        .text(names)
+
+var promise = d3.json("planets.json")
 
 
+
+promise.then(
+function(pimg)
+{
+    console.log("pimg", pimg);
+    return image(pimg)
+    
+},
+function(err)
+{
+    console.log("fail", err);
+}
+)
+
+var image = function(planets)
+{
+    d3.select("#B3")
+        .selectAll("img")
+        .data(planets)
+        .enter()
+        .append("img")
+        .attr("src", function(planets2)
+        {
+              return planets2.img;
+              })
+}
  
